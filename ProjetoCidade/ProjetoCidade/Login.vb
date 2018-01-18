144aa3c112842da716caa30839658a4dea28af63
thechefmiguel <35562498+thechefmiguel@users.noreply.github.com>
2018-01-18 15:53:42 +0000

    Login feito
    Finalizei a página de login, já é possível verificar se um utilizador está registado com password

--- ProjetoCidade/ProjetoCidade/Login.vb
+++ ProjetoCidade/ProjetoCidade/Login.vb
@@ -1,7 +1,30 @@
 ﻿Public Class Login
+    Dim Total As Integer
+    Dim utilizadores(99, 2) As String
 
     Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
-        Me.Hide()
-        Cidade.Show()
+        FileOpen(1, "E:\utilizadores.txt", OpenMode.Input)
+        Input(1, Total)
+        For i = 1 To Total
+            Input(1, utilizadores(i, 1))
+            Input(1, utilizadores(i, 2))
+        Next i
+        FileClose(1)
+
+        Dim utilizador As String = tbNome.Text
+        Dim password As String = tbPassword.Text
+
+        For i = 1 To Total
+            If utilizador = utilizadores(i, 1) Then
+                If password = utilizadores(i, 2) Then
+                    Me.Hide()
+                    Cidade.Show()
+                Else
+                    MsgBox("Password ou utilizador errado")
+                End If
+            Else
+                MsgBox("Password ou utilizador errado")
+            End If
+        Next i
     End Sub
 End Class
