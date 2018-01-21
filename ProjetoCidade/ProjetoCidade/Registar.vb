453041e22c17a1060b01da430494f87802df16c5
DriverSilva <12000kiko@gmail.com>
2018-01-21 22:48:24 +0000

    Alterações à form registar

--- ProjetoCidade/ProjetoCidade/Registar.vb
+++ ProjetoCidade/ProjetoCidade/Registar.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Registar
 
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        MsgBox("Ainda não está pronto")
+        'ler ficheiro
+        'escrever ficheiro
+    End Sub
 End Class
