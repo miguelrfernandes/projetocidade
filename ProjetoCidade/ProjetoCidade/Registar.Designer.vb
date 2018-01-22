dedf1b1d345c640ac3640c9ee14b4387f2368d05
thechefmiguel <35562498+thechefmiguel@users.noreply.github.com>
2018-01-22 22:34:49 +0000

    Revert "Acabado a página de Início e Registo"
    This reverts commit a273db96f03f0d805b3f722b573210c4eda3e429.

--- ProjetoCidade/ProjetoCidade/Registar.Designer.vb
+++ ProjetoCidade/ProjetoCidade/Registar.Designer.vb
@@ -25,7 +25,6 @@ Partial Class Registar
         Me.tbUser = New System.Windows.Forms.TextBox()
         Me.tbPassword = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'tbUser
@@ -48,24 +47,14 @@ Partial Class Registar
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Registar"
+        Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(152, 167)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Voltar"
-        Me.Button2.UseVisualStyleBackColor = True
         '
         'Registar
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 261)
-        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.tbPassword)
         Me.Controls.Add(Me.tbUser)
@@ -78,5 +67,4 @@ Partial Class Registar
     Friend WithEvents tbUser As System.Windows.Forms.TextBox
     Friend WithEvents tbPassword As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Button2 As System.Windows.Forms.Button
 End Class
